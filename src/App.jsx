c40c53ffd836256f733b30c77bfc5ab1196c3d81
matias <matias.vargas.m@usach.cl>
2024-01-22 09:01:35 -0600

    mejora

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Gallery from './components/Gallery'
+import Main from './components/Main'
+import Slider from './components/Slider'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='h-screen w-full flex justify-center'>
+    <div className='h-screen w-full flex justify-center min-w-screen'>
       <Navbar></Navbar>
+      <div className='flex items-center flex-col mt-40'>      <Main></Main>
+      <Gallery></Gallery>
+      <Slider></Slider>
+      </div>
     </div>
   )
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import adidas from "../assets/adidas.png"
+import aws from "../assets/aws.png"
+import netflix from "../assets/netflix.png"
+function Slider() {
+  return (
+    
+
+<div id="gallery" class="flex w-full" data-carousel="slide">
+
+</div>
+
+  )
+}
+
+export default Slider
