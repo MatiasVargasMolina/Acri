cc86f9eb6ab7b7d03dd9c36c2fa552c9f0c013d0
matias <matias.vargas.m@usach.cl>
2024-01-22 20:54:18 -0600

    nico dia trabajando

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,20 +5,23 @@ function Main() {
     
     
 
-<section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Diseña Con </span>PROFESIONALES</h1>
+<section className="bg-white dark:bg-gray-900">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+    <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Diseña Con </span>PROFESIONALES</h1>
 
-        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-¡Descubre la innovación en acrílicos con nosotros! Transformamos tus ideas en impactantes soluciones visuales. Haz clic ahora para obtener tu cotización personalizada y llevar tus proyectos al siguiente nivel.</p>
-        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+        <p className=" text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+        Con más de 35 años en el mercado, hemos perfeccionado el arte de transformar ideas en impactantes piezas visuales, para nosotros cada proyecto es una oportunidad para demostrar nuestra destreza y creatividad.
+</p>
+<p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">En ACRILICOS ELEPE, no solo creamos productos sino que tambien forjamos relaciones duraderas. 
+Nuestra dedicación a la calidad, atención al cliente y profesionalismo ha cimentado asociaciones sólidas con empresas líderes en diversos sectores, en el que somos la elección preferida para aquellos que buscan mas que a un proveedor, un aliado estratégico.</p>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Cotiza
-                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
             </a>
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <a href="#" className="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 Mas información
             </a>  
         </div>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import adidas from "../assets/adidas.png"
 import aws from "../assets/aws.png"
 import netflix from "../assets/netflix.png"
+import { motion } from "framer-motion"
 function Slider() {
-  return (
+    const imgSrcArray=[aws,aws,netflix,netflix]
+const [posicion,setPosicion]=useState(0)
+const [positionX,setPositionX]=useState(0)
+const cambio =()=>{
+    setPosicion((posicion==imgSrcArray.length-1)?0:posicion+1)
+    setPositionX((positionX==500)?0:500)
+}
+    useEffect(() => {
+        // Inicializar el intervalo
+        const intervalo = setInterval(() => {
+          // Lógica que se ejecutará repetidamente
+          cambio();
+        }, 3000); // Ejemplo: se ejecuta cada segundo (1000 ms)
     
+        // Limpiar el intervalo al desmontar el componente
+        return () => clearInterval(intervalo);
+      }, [cambio]);
+  return (
 
-<div id="gallery" class="flex w-full" data-carousel="slide">
 
+<div id="gallery" className='flex items-center justify-center w-full' data-carousel="slide">
+
+<motion.div animate={{ x: positionX }} transition={{
+  ease: "linear",
+  duration: 2,
+}} className='w-full h-full mr-10 'style={{width:"300px",height:"400px"}}>
+    <img  src={imgSrcArray[posicion]} />
+</motion.div>
 </div>
 
   )
