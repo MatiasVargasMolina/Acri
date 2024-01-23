5fc9401f4df0ae8b3f0a820df4c8aa77ee7650fe
matias <matias.vargas.m@usach.cl>
2024-01-22 21:01:21 -0600

    cambios

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,7 +16,7 @@ function Main() {
 Nuestra dedicación a la calidad, atención al cliente y profesionalismo ha cimentado asociaciones sólidas con empresas líderes en diversos sectores, en el que somos la elección preferida para aquellos que buscan mas que a un proveedor, un aliado estratégico.</p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                Cotiza
+            Contáctate con nosotros
                 <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
